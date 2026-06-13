d48bb2bd62671a2da2486b55db6bfeb3d3ae1c8a
Sumit Singh <singhsumit.k07@gmail.com>
2026-06-13 21:14:15 +0000

    Updated the Transformation stage

--- transform.py
+++ transform.py
@@ -9,23 +9,46 @@ def transform_weather_data():
 		print (i)
 		time.sleep(0.5)
 
-	# Opening the Bronze layer file
-	with open("raw_weather_data.json","r") as file:
-		raw_data = json.load(file)
+	try:
+		# Opening the Bronze layer file
+		with open("raw_weather_data.json","r") as file:
+			raw_data = json.load(file)
 
-	# We just need the 'hourly' dictionary
-	hourly_forecast = raw_data['hourly']
+		lat= raw_data.get("latitude")
+		lon= raw_data.get("longitude")
+		tz=raw_data.get("timezone","Unknown")
 
-	#injecting the dictionary into pandas to flatten it
-	df = pd.DataFrame(hourly_forecast)
+		# Generating the Primary/Foreign key mapping
+		location_id = f"{lat}_{lon}"
+		print(f"Extracted Geographic Context: Loaction ID [{location_id}]")
 
-	print("Data flattening successful")
-	print("_"*39)
-	print(df.head())
-	df.to_parquet('silver_weather_data.paraquet',index=False)
-	print("Checkpoint reached...Silver layer created")
+		if 'hourly' not in raw_data:
+			raise KeyError("The 'hourly' data block is missing from the payload")
+		df=pd.DataFrame(raw_data['hourly'])
 
-	return df
+
+		df['loaction_id'] = location_id
+		df['latitude']=lat
+		df['longitude']=lon
+		df['timezone']=tz
+
+
+
+		print("Data flattening successful")
+		print("_"*39)
+		print(df.head())
+		df.to_parquet('silver_weather_data.parquet',index=False)
+		print("Checkpoint reached...Silver layer created")
+
+
+
+
+	except FileNotFoundError:
+		print("CRITICAL ERROR: raw data file not found, run transform.py first")
+
+	except Exception as e:
+		print(f"CRITICAL ERROR: Error occured during tranformation - {e}")
+
 
 
 transform_weather_data()
